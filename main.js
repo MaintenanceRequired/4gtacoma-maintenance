7cd9fed9c3201d7a2ffa88bf59129a0a7a4132de
MaintenanceRequired <quiksilver601@msn.com>
2025-07-29 20:40:26 -0600

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  window.showSection = function(id) {
-    document.querySelectorAll("main section").forEach(section => {
-      section.classList.remove("active");
-    });
-    document.getElementById(id).classList.add("active");
-  };
-});
