b6fbebde549f9e4e753716da4b33ae26704765cd
MaintenanceRequired <quiksilver601@msn.com>
2025-07-29 20:31:11 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
-function showSection(id) {
-  document.querySelectorAll("main section").forEach(section => {
-    section.classList.remove("active");
-  });
-  document.getElementById(id).classList.add("active");
+document.addEventListener("DOMContentLoaded", () => {
+  window.showSection = function(id) {
+    document.querySelectorAll("main section").forEach(section => {
+      section.classList.remove("active");
+    });
+    document.getElementById(id).classList.add("active");
+  };
+});
+active");
 }
